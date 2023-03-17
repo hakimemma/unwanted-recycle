68c3e772fde4967793f9280fcab3fd721f671bb8
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-03-17 15:21:46 +0000

    scene changer

---
+++ Assets/Scenes/trash.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class trash : MonoBehaviour
+{
+    public int requiredItems = 10;
+    public TMPro.TextMeshProUGUI counterProgess;
+    public int unwantedItems = 0;
+    public int nextLevel = 0;
+    public string counterText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        unwantedItems += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterText = "";
+        counterText += unwantedItems.ToString();
+        counterText += "/";
+        counterText += requiredItems.ToString();
+        counterText += " items ";
+
+        counterProgess.text = counterText;
+
+        if(unwantedItems >= requiredItems)
+        {
+            Debug.Log("FHFHFF");
+            SceneManager.LoadScene(nextLevel);
+        } 
+    }
+}
