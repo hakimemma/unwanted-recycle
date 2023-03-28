7d1de8a439e12cf184f4857f96da2fecc283e844
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-03-28 18:02:03 +0100

    mike scene done

---
+++ Assets/Scenes/idk.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class idk : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void retry()
+    {
+        SceneManager.LoadScene("conv2");
+    }
+
+}
