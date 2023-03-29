83d5916a054b0924b3e39041652dcb9b938b6466
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-03-29 14:45:50 +0100

    ewsefdewf

---
+++ Assets/Scenes/scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class w : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void retry()
+    {
+        SceneManager.LoadScene("rc2");
+    }
+
+}
+
---
+++ Assets/Scenes/scripts/lvlboss.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class lvlboss : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void retry()
+    {
+        SceneManager.LoadScene("rc2");
+    }
+
+}
+
+
